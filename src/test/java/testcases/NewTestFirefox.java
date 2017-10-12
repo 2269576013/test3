6e44aa4b7dae9f95011eecfa3089283060caad87
2269576013 <2269576013@qq.com>
2017-10-12 16:34:33 +0800

    4

--- src/test/java/testcases/NewTestFirefox.java
+++ src/test/java/testcases/NewTestFirefox.java
@@ -2,16 +2,21 @@ package testcases;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
 import static org.testng.Assert.*;
+
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import utility.Log;
 
 public class NewTestFirefox {
 	WebDriver driver;
   @Test
   public void f() {
 	  System.out.println("fffffffffffffffffffffff");
+	  Log.info("11111111111111111111111111");
 	  assertTrue(true);
   }
   @BeforeMethod
--- src/test/java/testcases/NewTestMsg.java
+++ src/test/java/testcases/NewTestMsg.java
@@ -43,6 +43,7 @@ public class NewTestMsg {
     Log.info("Assert page title");
   }
 
+  
   @AfterClass(alwaysRun = true)
   public void tearDown() throws Exception {
     driver.quit();
